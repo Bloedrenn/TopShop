def063b430a18470a2f272805049f26b71f4d424
Bloedrenn <bloedrenn@gmail.com>
2026-08-02 15:05:45 +0300

    commit 11

--- login.py
+++ login.py
@@ -1,7 +1,6 @@
 def run_login():
     print("\n--- СТРАНИЦА ВХОДА ---")
     username = input("Введите: ").strip()
-    password = input("пароль: ").strip()
 
     # Проверяем данные по нашей общей базе
     if username:
